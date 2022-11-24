1a15540582548449a6aa3265eb3801d2866d6141
Sumon Kaysar <msk.sumon.kaysar@gmail.com>
2022-11-24 21:24:14 +0600

    added mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const jwt = require("jsonwebtoken")
+const { MongoClient, ServerApiVersion } = require('mongodb')
 require("dotenv").config()
 
 const port = process.env.PORT || 5000
@@ -9,6 +10,11 @@ const app = express()
 // middle ware
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zuoxzfe.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+
 
 app.get("/", (req, res) => {
   res.send("Used Cars Mart server is running")
